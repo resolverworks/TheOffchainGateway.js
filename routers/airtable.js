1c92651b9fb1a987fdb45cf923ff7bda41967dad
Andrew Raffensperger <raffy@me.com>
2024-02-18 23:31:29 -0800

    rename air slug

--- routers/airtable.js
+++ routers/airtable.js
@@ -4,7 +4,7 @@ import {AirtableRouter} from '../src/AirtableRouter.js';
 // expects columns: [name, address]
 
 export default new AirtableRouter({
-	slug: 'air',
+	slug: 'airtable',
 	secret: 'pat74noP03o6JK2ic.b0023a05e3b417174aaf3bff6325014166a4afcf6e47a5bdb470cfb33ceeb36e',
 	base: 'appzYI39knUZdO88N'
 });
