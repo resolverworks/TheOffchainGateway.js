ff5dd4be6a39cfb6d1f6d20ef6da5f19601539e0
Andrew Raffensperger <raffy@me.com>
2024-02-25 21:37:57 -0800

    just search for label

--- routers/github.js
+++ routers/github.js
@@ -19,32 +19,30 @@ export default Router.from({
 		// this is a hack since to work as demo
 		// since this could be hosted under anyones domain
 		let parts = name.split('.');
-		if (parts.length >= 5) { 
-			parts.pop(); // "eth"
-			parts.pop(); // "debug"	
-			parts.pop(); // "github"
-			// [sub][user][repo]
-			for (let n = 1; n <= 2; n++) {
-				if (n == parts.length) break;
-				let repo = parts.slice(parts.length - n).join('.');
-				let user = parts[parts.length - (n + 1)];
-				let rest = parts.slice(0, -(n + 1)).join('.');
-				let path = `${user}/${repo}`;
-				try {
-					let root = await cache.get(path, 10000, async () => {
-						let res = await fetch(`https://github.com/${user}/${repo}/raw/main/ENS.json`);
-						if (!res.ok) throw new Error(`http ${res.status}`);
-						let json = await res.json();
-						let root = Node.root();
-						root.import_from_json(json);
-						return root;
-					});	
-					this.log(`${path} => ${rest}`);
-					let node = root.find(rest);
-					return node?.rec;
-				} catch (err) {
-					this.log(`no repo: ${path} ${err.message}`)
-				}
+		let index = parts.lastIndexOf('github');
+		if (index == -1) return;
+		parts = parts.slice(0, index);
+		// [sub][user][repo]
+		for (let n = 1; n <= 2; n++) {
+			if (n == parts.length) break;
+			let repo = parts.slice(parts.length - n).join('.');
+			let user = parts[parts.length - (n + 1)];
+			let rest = parts.slice(0, -(n + 1)).join('.');
+			let path = `${user}/${repo}`;
+			try {
+				let root = await cache.get(path, 10000, async () => {
+					let res = await fetch(`https://github.com/${user}/${repo}/raw/main/ENS.json`);
+					if (!res.ok) throw new Error(`http ${res.status}`);
+					let json = await res.json();
+					let root = Node.root();
+					root.import_from_json(json);
+					return root;
+				});	
+				this.log(`${path} => ${rest}`);
+				let node = root.find(rest);
+				return node?.rec;
+			} catch (err) {
+				this.log(`no repo: ${path} ${err.message}`)
 			}
 		}
 		return record0;
