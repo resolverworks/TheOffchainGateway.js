437870e32fdab341578beef6ca3acd551be2d59c
Andrew Raffensperger <raffy@me.com>
2024-02-29 01:31:51 -0800

    fix github demo again sigh

--- routers/github.js
+++ routers/github.js
@@ -61,7 +61,8 @@ export default Router.from({
 			root.import_from_json(json);
 			if (!root.rec) root.rec = new Record(); // rare
 			if (!root.rec.has('com.github')) root.rec.set('com.github', account);
-			if (!root.rec.has('url')) root.rec.set(`https://github.com/${account === repo ? account : path}`);
+			if (!root.rec.has('url')) root.rec.set('url', `https://github.com/${account === repo ? account : path}`);
+			console.log(root.rec);
 			return root;
 		});
 	}
