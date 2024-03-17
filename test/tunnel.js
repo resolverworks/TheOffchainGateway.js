e079882be39e6027c407c1b370e7f2ac531d9d2a
Andrew Raffensperger <raffy@me.com>
2024-03-17 14:55:35 -0700

    change tunnel demo

--- test/tunnel.js
+++ test/tunnel.js
@@ -1,12 +1,18 @@
-import {InfuraProvider, ethers} from 'ethers';
+import {ethers} from 'ethers';
+import {flat_json} from '../routers/tunnel.js';
+
+console.log(flat_json({a: 1, b: [2, "chonk"]}));
 
 // see: routers/tunnel.js
 // https://sepolia.etherscan.io/address/0xCa71342cB02714374e61e400f172FF003497B2c2
-let provider = new InfuraProvider(11155111);
+let provider = new ethers.InfuraProvider(11155111);
 let contract = new ethers.Contract('0xCa71342cB02714374e61e400f172FF003497B2c2', [
 	'function fetchFlatJSON(string url) view returns (tuple(string, string)[])'
 ], provider);
 
-let res = await contract.fetchFlatJSON('https://raw.githubusercontent.com/resolverworks/TheOffchainGateway.js/main/package.json', {enableCcipRead: true});
+let res = await contract.fetchFlatJSON('https://api.gmcafe.io/metadata/gmoo/331.json', {enableCcipRead: true});
 
-console.log(res);
+for (let x of res) {
+	console.log(x.toArray());
+}
+
